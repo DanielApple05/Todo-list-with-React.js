5d0d865b6766779cba3db9edfa5c64d590f38af5
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-03 20:59:21 +0100

    fix: update Todo component to include sun icon and improve layout styling

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import todo_icon from '../assets/todo_icon.svg';
 import TodoItems from './TodoItems.jsx';
 import moonIcon from '../../public/images/icon-moon.svg';
+import sunIcon from '../../public/images/icon-moon.svg'
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 // import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
@@ -40,30 +41,24 @@ const Todo = () => {
     })
   }
 
+  const { darkMode, lightMode} = useState (darkMode)
+
   useEffect(() => { localStorage.setItem("todos", JSON.stringify(todoList)) }, [todoList])
 
   return (
     <div>
-
       <div className='flex items-center mt-7 gap-2 place-self-center w-[40%] mb-8  rounded-xl justify-between'>
-
         <h1 className=' text-white text-3xl font-semibold'>To-Do List</h1>
-
-
-        <div className='cursor-pointer'>
+        <div className='cursor-pointer' onClick={ darkMode ? moonIcon : sunIcon }>
           <img src={moonIcon} alt="" />
         </div>
-
       </div>
-
       <div className=' place-self-center w-[40%] flex flex-col  min-h-[550px] rounded-xl'>
-
         <div className='flex items-center mb-7 bg-gray-200 rounded-lg'>
           <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder=' create a new todo...' />
 
           <button onClick={add} className='border-none rounded-lg bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
-        </div>
-        <div>
+        </div>      
           <div className='bg-white flex  flex-col flex-1'>
             {todoList.map((item, index) => (
               <TodoItems
@@ -77,7 +72,7 @@ const Todo = () => {
             ))}
           </div>
 
-          <div className='flex justify-between bg-white'>
+          <div className='flex justify-between bg-white p-2'>
             <div>
               <p>
                 5 items left
@@ -94,7 +89,6 @@ const Todo = () => {
               </p>
             </div>
           </div>
-        </div>
       </div>
     </div>
   );
