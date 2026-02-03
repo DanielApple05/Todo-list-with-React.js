bf339fb8ef586b2752886a54b571a4375b8e774c
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-03 23:05:32 +0100

    updates

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -40,8 +40,7 @@ const Todo = () => {
       })
     })
   }
-
-  const { darkMode, lightMode} = useState (darkMode)
+ const [darkMode, setDarkMode] = useState();
 
   useEffect(() => { localStorage.setItem("todos", JSON.stringify(todoList)) }, [todoList])
 
@@ -49,46 +48,50 @@ const Todo = () => {
     <div>
       <div className='flex items-center mt-7 gap-2 place-self-center w-[40%] mb-8  rounded-xl justify-between'>
         <h1 className=' text-white text-3xl font-semibold'>To-Do List</h1>
-        <div className='cursor-pointer' onClick={ darkMode ? moonIcon : sunIcon }>
-          <img src={moonIcon} alt="" />
+        <div
+          className="cursor-pointer"
+          onClick={() => setDarkMode(prev => !prev)}
+        >
+          <img src={darkMode ? moonIcon : sunIcon} alt="toggle theme" />
         </div>
+
       </div>
       <div className=' place-self-center w-[40%] flex flex-col  min-h-[550px] rounded-xl'>
         <div className='flex items-center mb-7 bg-gray-200 rounded-lg'>
           <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder=' create a new todo...' />
 
           <button onClick={add} className='border-none rounded-lg bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>ADD +</button>
-        </div>      
-          <div className='bg-white flex  flex-col flex-1'>
-            {todoList.map((item, index) => (
-              <TodoItems
-                key={item.id + index}
-                text={item.text}
-                id={item.id}
-                isComplete={item.isComplete}
-                deleteTodo={deleteTodo}
-                toggle={toggle}
-              />
-            ))}
-          </div>
+        </div>
+        <div className='bg-white flex  flex-col flex-1'>
+          {todoList.map((item, index) => (
+            <TodoItems
+              key={item.id + index}
+              text={item.text}
+              id={item.id}
+              isComplete={item.isComplete}
+              deleteTodo={deleteTodo}
+              toggle={toggle}
+            />
+          ))}
+        </div>
 
-          <div className='flex justify-between bg-white p-2'>
-            <div>
-              <p>
-                5 items left
-              </p>
-            </div>
-            <div className='flex space-x-6'>
-              <p>All</p>
-              <p>Active</p>
-              <p>Completed</p>
-            </div>
-            <div>
-              <p>
-                Clear Completed
-              </p>
-            </div>
+        <div className='flex justify-between bg-white p-2'>
+          <div>
+            <p>
+              5 items left
+            </p>
           </div>
+          <div className='flex space-x-6'>
+            <p>All</p>
+            <p>Active</p>
+            <p>Completed</p>
+          </div>
+          <div>
+            <p>
+              Clear Completed
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
