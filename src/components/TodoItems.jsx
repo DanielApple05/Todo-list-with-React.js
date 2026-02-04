d77c7a26f1c2faba4a9d776916801477a4b15ee6
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-04 14:29:47 +0100

    commit

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -7,9 +7,9 @@ const TodoItems = ({ text, id, isComplete, deleteTodo, toggle, }) => {
   return (
     <div className='flex space-y-6 border-b justify-between px-4 rounded items-center '>
 
-      <div onClick={() => { toggle(id) }} className='flex cursor-pointer items-center pt-4'>
+      <div className='flex cursor-pointer items-center pt-4'>
 
-        <FontAwesomeIcon icon={isComplete ? faCircleCheck : faCircleRegular} className={`text-[20px] hover:text-purple-500 transition-color ${isComplete ? "text-purple-500" : "text-gray-400"}`} />
+        <FontAwesomeIcon icon={isComplete ? faCircleCheck : faCircleRegular} className={`text-[20px] hover:text-purple-500 transition-colors ${isComplete ? "text-purple-500" : "text-gray-400"}`} onClick={() => { toggle(id) }} />
 
         <p className={`ml-4 text-[19px] decoration-slate-100 ${isComplete ? "text-red-500 line-through" : ""}`}>
           {text}
