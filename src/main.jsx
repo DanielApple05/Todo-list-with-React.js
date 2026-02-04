4325b99f7627c3f998d2cd439c3554aeb411f6eb
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-04 16:26:01 +0100

    more commits

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
