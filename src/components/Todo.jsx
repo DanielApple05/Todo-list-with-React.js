8ebad02bdf2754ee0b03bedd03fb93cebfe9fcdf
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-24 15:17:16 +0100

    ui fixes

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -63,14 +63,17 @@ const Todo = () => {
   }, [darkMode]);
 
   return (
-    <div className='max-w-12/12 relative'>
-      <div className={` overflow-hidden bg-no-repeat flex place-content-center h-screen 
-            ${darkMode
-          ? "bg-[#1c2033] bg-[url('/images/bg-desktop-dark.jpg')]"
-          : "bg-white bg-[url('/images/bg-desktop-light.jpg')]"
-        } ${darkMode ? "text-white" : "text-black"}`}>
-        <div className=' xl:max-w-5/12 w-full place-self-center xl:mt-4 mt-10 grid'>
-          <div className='flex items-center xl:mt-7 mt-3 xl:mb-6 mb-15 justify-between xl:px-block px-5 fixed top-15 left-0 right-0'>
+    <div className={` ${darkMode ? "text-white bg-black" : " bg-white text-black"}`} >
+      <section
+  className={`fixed top-0 left-0 w-full min-w-12/12 place-content-center xl:h-70 h-40 bg-cover bg-no-repeat flex z-50
+  ${
+    darkMode
+      ? "bg-[url('/images/bg-desktop-dark.jpg')]"
+      : "bg-[url('/images/bg-desktop-light.jpg')]"
+  }`}
+>
+        <div className=' flex flex-col xl:max-w-5/12 w-full place-self-center xl:mt-10 mt-15'>
+          <div className='flex items-center xl:mt-7 mt-3 xl:mb-6 mb-15 justify-between xl:px-0 px-5'>
             <h1 className=' text-white xl:text-3xl text-xl font-semibold '>To-Do List</h1>
             <div
               className="cursor-pointer"
@@ -79,83 +82,83 @@ const Todo = () => {
               <img src={darkMode ? sunIcon : moonIcon} alt="themeMode" className='xl:w-7 w-4' />
             </div>
           </div>
-          <div className={`flex items-center xl:mb-10 mb-15 rounded-lg xl:mx-0 mx-5 fixed top-50 left-0 right-0 ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
+          <div className={`flex items-center xl:mb-10 mb-15 rounded-lg xl:mx-0 mx-5  ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
             <input ref={inputRef} className=' border-0 outline-none flex-1 xl:h-14 h-10 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='create a new todo...' />
 
             <button onClick={add} className='border-none rounded-lg bg-[#b266ff] xl:w-32 w-20 xl:h-14 h-10 text-white xl:text-lg text-sm font-medium cursor-pointer hover:bg-blue-500'>ADD +</button>
           </div>
+        </div>
+      </section>
+      <div className=' xl:max-w-5/12 w-full place-self-center xl:mt-70 mt-40 grid overflow-y-auto'>
+        <div className={`flex flex-col shadow-2xl  w-full  ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
+          {filteredTodos.map((item, index) => (
+            <TodoItems
+              key={item.id}
+              text={item.text}
+              id={item.id}
+              isComplete={item.isComplete}
+              deleteTodo={deleteTodo}
+              toggle={toggle}
+            />
+          ))}
+        </div>
+        <div className={`flex justify-between p-4 shadow-xl border-t z-50 fixed w-full left-0 right-0 bottom-0 xl:text-[16px] text-[10px] 
+            ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
+          <div>
+            <p>
+              {itemsLeft} {itemsLeft === 1 ? "item" : "items"} left
+            </p>
+          </div>
+          <div className="flex xl:space-x-6 space-x-2 ">
+            <p
+              onClick={() => setFilter("all")}
+              className={`cursor-pointer transition-colors ${filter === "all"
+                ? "font-bold text-blue-500"
+                : darkMode
+                  ? "text-gray-400 hover:text-white"
+                  : "text-gray-500 hover:text-black"
+                }`}
+            >
+              All
+            </p>
+            <p
+              onClick={() => setFilter("active")}
+              className={`cursor-pointer transition-colors ${filter === "active"
+                ? "font-bold text-blue-500"
+                : darkMode
+                  ? "text-gray-400 hover:text-white"
+                  : "text-gray-500 hover:text-black"
+                }`}
+            >
+              Active
+            </p>
 
-          <div className={`flex flex-col shadow-2xl xl:h-130 h-130 fixed top-80 w-full overflow-y-auto  `}>
-            {filteredTodos.map((item, index) => (
-              <TodoItems
-                key={item.id}
-                text={item.text}
-                id={item.id}
-                isComplete={item.isComplete}
-                deleteTodo={deleteTodo}
-                toggle={toggle}
-              />
-            ))}
+            <p
+              onClick={() => setFilter("completed")}
+              className={`cursor-pointer transition-colors ${filter === "completed"
+                ? "font-bold text-blue-500"
+                : darkMode
+                  ? "text-gray-400 hover:text-white"
+                  : "text-gray-500 hover:text-black"
+                }`}
+            >
+              Completed
+            </p>
+
           </div>
 
+          <div>
+            <p
+              onClick={clearCompleted}
+              className="cursor-pointer text-red-500 hover:underline"
+            >
+              Clear Completed
+            </p>
 
-          <div className={`flex justify-between p-4 shadow-xl border-t z-50 fixed w-full left-0 right-0 bottom-0 xl:text-[16px] text-[10px] 
-            ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
-            <div>
-              <p>
-                {itemsLeft} {itemsLeft === 1 ? "item" : "items"} left
-              </p>
-            </div>
-            <div className="flex xl:space-x-6 space-x-2 ">
-              <p
-                onClick={() => setFilter("all")}
-                className={`cursor-pointer transition-colors ${filter === "all"
-                  ? "font-bold text-blue-500"
-                  : darkMode
-                    ? "text-gray-400 hover:text-white"
-                    : "text-gray-500 hover:text-black"
-                  }`}
-              >
-                All
-              </p>
-              <p
-                onClick={() => setFilter("active")}
-                className={`cursor-pointer transition-colors ${filter === "active"
-                  ? "font-bold text-blue-500"
-                  : darkMode
-                    ? "text-gray-400 hover:text-white"
-                    : "text-gray-500 hover:text-black"
-                  }`}
-              >
-                Active
-              </p>
-
-              <p
-                onClick={() => setFilter("completed")}
-                className={`cursor-pointer transition-colors ${filter === "completed"
-                  ? "font-bold text-blue-500"
-                  : darkMode
-                    ? "text-gray-400 hover:text-white"
-                    : "text-gray-500 hover:text-black"
-                  }`}
-              >
-                Completed
-              </p>
-
-            </div>
-
-            <div>
-              <p
-                onClick={clearCompleted}
-                className="cursor-pointer text-red-500 hover:underline"
-              >
-                Clear Completed
-              </p>
-
-            </div>
           </div>
         </div>
       </div>
+
 
     </div>
   );
