7286047de7aea88c170410e99dfeae325499d991
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-02-24 13:36:05 +0100

    ui update

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -63,14 +63,14 @@ const Todo = () => {
   }, [darkMode]);
 
   return (
-    <div className='max-w-12/12 '>
-      <div className={` overflow-hidden bg-no-repeat flex place-content-center h-screen
+    <div className='max-w-12/12 relative'>
+      <div className={` overflow-hidden bg-no-repeat flex place-content-center h-screen 
             ${darkMode
           ? "bg-[#1c2033] bg-[url('/images/bg-desktop-dark.jpg')]"
           : "bg-white bg-[url('/images/bg-desktop-light.jpg')]"
         } ${darkMode ? "text-white" : "text-black"}`}>
         <div className=' xl:max-w-5/12 w-full place-self-center xl:mt-4 mt-10 grid'>
-          <div className='flex items-center xl:mt-7 mt-3 xl:mb-6 mb-15 justify-between xl:px-block px-5'>
+          <div className='flex items-center xl:mt-7 mt-3 xl:mb-6 mb-15 justify-between xl:px-block px-5 fixed top-15 left-0 right-0'>
             <h1 className=' text-white xl:text-3xl text-xl font-semibold '>To-Do List</h1>
             <div
               className="cursor-pointer"
@@ -79,13 +79,13 @@ const Todo = () => {
               <img src={darkMode ? sunIcon : moonIcon} alt="themeMode" className='xl:w-7 w-4' />
             </div>
           </div>
-          <div className={`flex items-center xl:mb-10 mb-15 rounded-lg xl:px-0 px-5 ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
+          <div className={`flex items-center xl:mb-10 mb-15 rounded-lg xl:mx-0 mx-5 fixed top-50 left-0 right-0 ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
             <input ref={inputRef} className=' border-0 outline-none flex-1 xl:h-14 h-10 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='create a new todo...' />
 
             <button onClick={add} className='border-none rounded-lg bg-[#b266ff] xl:w-32 w-20 xl:h-14 h-10 text-white xl:text-lg text-sm font-medium cursor-pointer hover:bg-blue-500'>ADD +</button>
           </div>
 
-          <div className={`flex flex-col shadow-2xl xl:h-130 h-90 w-full overflow-y-auto  `}>
+          <div className={`flex flex-col shadow-2xl xl:h-130 h-130 fixed top-80 w-full overflow-y-auto  `}>
             {filteredTodos.map((item, index) => (
               <TodoItems
                 key={item.id}
