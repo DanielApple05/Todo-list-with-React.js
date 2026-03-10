c5674376e71a4b0c0e8629512f6e4522b2501cdc
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-03-10 22:03:52 +0100

    UI FIXES

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -63,7 +63,7 @@ const Todo = () => {
 
   return (
     <div className={` w-full relative ${darkMode ? "text-white bg-black" : " bg-white "}`} >
-      <section className={` sticky top-0 left-0 w-full min-w-12/12 place-content-center xl:h-40 h-45 bg-cover bg-no-repeat flex z-50 
+      <section className={` sticky top-0 left-0 w-full min-w-12/12 place-content-center xl:h-40 h-45 bg-cover bg-no-repeat flex z-50  xl:text-[16px] text-[14px]  
           ${darkMode
           ? "bg-[url('/images/bg-desktop-dark.jpg')]"
           : "bg-[url('/images/bg-desktop-light.jpg')]"
@@ -96,7 +96,7 @@ const Todo = () => {
           />
         ))}
       </div>
-      <div className={` xl:w-5/12 w-full flex justify-between p-4 shadow-xl border-t z-50 sticky place-self-center bottom-0 xl:text-[16px] text-[10px] 
+      <div className={` xl:w-5/12 w-full flex justify-between p-4 shadow-xl border-t z-50 sticky place-self-center bottom-0 xl:text-[14px] text-[10px] 
             ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
         <div>
           <p>
--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -5,15 +5,13 @@ import { faCircleCheck, faTrash } from "@fortawesome/free-solid-svg-icons";
 
 const TodoItems = ({ text, id, isComplete, deleteTodo, toggle, }) => {
   return (
-    <div className='flex xl:space-y-6 space-y-2 border-b justify-between px-4 rounded items-center '>
-
+    <div className='flex xl:space-y-4 space-y-2 border-b justify-between px-4 rounded items-center  xl:text-[16px] text-[14px] '>
       <div className='flex items-center pt-4 '>
-
         <FontAwesomeIcon icon={isComplete ? faCircleCheck : faCircleRegular} 
         onClick={() => { toggle(id) }} 
-        className={`xl:text-[20px] text-[14px] hover:text-[#b266ff] transition-colors ${isComplete ? "text-[#b266ff]" : "text-gray-400"}`} />
+        className={`cursor-pointer hover:text-[#b266ff] transition-colors ${isComplete ? "text-[#b266ff]" : "text-gray-400"}`} />
 
-        <p className={`xl:ml-4 ml-2 xl:text-[19px] text-[14px] decoration-slate-100 ${isComplete ? "text-red-500 line-through" : ""}`}>
+        <p className={`xl:ml-4 ml-2 decoration-slate-100 ${isComplete ? "text-red-500 line-through" : ""}`}>
           {text}
         </p>
       </div>
