da4ad206afd68f8b318fd9707db2e7c8992e6bd7
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-03-10 21:43:28 +0100

    UI update

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -70,23 +70,23 @@ const Todo = () => {
           ? "bg-[url('/images/bg-desktop-dark.jpg')]"
           : "bg-[url('/images/bg-desktop-light.jpg')]"
         }`}>
-        <div className=' flex flex-col xl:max-w-5/12 w-full place-self-center xl:mt-10 mt-15'>
+        <div className=' flex flex-col xl:min-w-5/12 min-w-full place-self-center xl:mt-10 mt-15'>
           <div className='flex items-center xl:mt-7 mt-3 xl:mb-6 mb-15 justify-between xl:px-0 px-5'>
-            <h1 className=' text-white xl:text-3xl text-xl font-semibold '>To-Do List</h1> 
+            <h1 className=' text-white xs:text-3xl text-xl font-semibold '>To-Do List</h1> 
             <div
               className="cursor-pointer"
               onClick={() => setDarkMode(!darkMode)}>
               <img src={darkMode ? sunIcon : moonIcon} alt="themeMode" className='xl:w-7 w-4' />
             </div>
           </div>
-          <div className={`flex items-center xl:mb-10 mb-15 rounded-lg xl:mx-0 mx-5  ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
-            <input ref={inputRef} className=' border-0 outline-none flex-1 xl:h-14 h-10 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='create a new todo...' />
+          <div className={`flex items-center mb-15 rounded-lg xl:mx-0 mx-5  ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
+            <input ref={inputRef} className=' border-0 outline-none flex-1 xl:h-12 h-10 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='create a new todo...' />
 
-            <button onClick={add} className='border-none rounded-lg bg-[#b266ff] xl:w-32 w-20 xl:h-14 h-10 text-white xl:text-lg text-sm font-medium cursor-pointer hover:bg-blue-500'>ADD +</button>
+            <button onClick={add} className='border-none rounded-lg bg-[#b266ff] xl:w-32 w-20 xl:h-12 h-10 text-white xl:text-lg text-sm font-medium cursor-pointer hover:bg-blue-500'>ADD +</button>
           </div>
         </div>
       </section>
-      <div className={`xl:max-w-5/12 w-full mx-auto min-h-screen shadow-2xl mb-10 ${darkMode ? "text-white bg-[#1e223c]" : "bg-white"}`}>
+      <div className={`xl:max-w-5/12 w-full mx-auto min-h-screen shadow-2xl ${darkMode ? "text-white bg-[#1e223c]" : "bg-white"}`}>
         {filteredTodos.map((item) => (
           <TodoItems
             key={item.id}
@@ -98,7 +98,7 @@ const Todo = () => {
           />
         ))}
       </div>
-      <div className={` xl:m-w-5/12 min-w-full flex justify-between p-4 shadow-xl border-t z-50 fixed place-self-center bottom-0 xl:text-[16px] text-[10px] 
+      <div className={` xl:m-w-5/12 min-w-full flex justify-between p-4 shadow-xl border-t z-50 sticky  bottom-0 xl:text-[16px] text-[10px] 
             ${darkMode ? "bg-[#1e223c]" : "bg-white"}`}>
         <div>
           <p>
