6af79bc8ddd5f3c2960c40c317bb31d9e9655342
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-03-27 11:37:52 +0100

    feat: add nodemon for development and setup basic Express server

---
+++ todo-backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("API is working");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
