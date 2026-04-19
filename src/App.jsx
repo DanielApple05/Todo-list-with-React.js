bd9c325018451aaeeee9a11020f5948a71919aa7
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-04-19 14:52:58 +0100

    feat: add login component and routing, update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
 import Todo from "./components/Todo";
 import '../src/App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/login";
+
 
 const App = () => {
   return (
-    <div>
-      <Todo/>
-    </div>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
   );
 };
 
---
+++ src/components/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const login = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-[url('../images/login-background.png')] bg-cover bg-no-repeat">
+      
+    </div>
+  );
+}
+
+export default login;
