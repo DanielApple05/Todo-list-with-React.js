9fa2f2b1c7fc4f618737954f41092055ebff709b
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-04-19 15:14:45 +0100

    feat: implement login component with state management and UI elements

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
 const login = () => {
+  const [loggedIn, setLoggedIn] = React.useState(false);
   return (
     <div className="flex items-center justify-center h-screen bg-[url('../images/login-background.png')] bg-cover bg-no-repeat">
-      
+      <div className='bg-white p-10 rounded-lg shadow-xl w-5/12 h-2.5/4 '>
+        <h2 className='text-2xl font-bold text-center'>
+          {loggedIn ? "Welcome Back!" : "Please Log In"}
+        </h2>
+        <form className='grid gap-3 my-5 '>
+          <input type="text" placeholder='Username' className='border border-gray-300 rounded-md p-2 w-full mb-4' />
+          <input type="password" placeholder='Password' className='border border-gray-300 rounded-md p-2 w-full mb-4' />
+          <button onClick={() => setLoggedIn(true)} className='bg-purple-500 text-white py-2 px-4 rounded-md w-full cursor-pointer'>Log In</button>
+        </form>
+        <p>
+          Don't have an account? <span className='text-blue-500 cursor-pointer'>Sign Up</span>
+        </p>
+      </div>
+
     </div>
   );
 }
